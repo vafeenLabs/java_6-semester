abd9ab7033d4e1e802e2e474aa80749bbace5405
Vafeen <666av6@gmail.com>
2025-02-18 18:57:59 +0300

    Add getters for SomeBean

--- src/main/java/org/example/lab5/SomeBean.java
+++ src/main/java/org/example/lab5/SomeBean.java
@@ -3,11 +3,30 @@ package org.example.lab5;
 class SomeBean {
     @AutoInjectable
     private SomeInterface field1;
+
     @AutoInjectable
     private SomeOtherInterface field2;
 
     public void foo() {
         field1.doSomething();
         field2.doSomeOther();
+    }
+
+    /**
+     * Геттер для поля field1.
+     *
+     * @return значение поля field1.
+     */
+    public SomeInterface getField1() {
+        return field1;
+    }
+
+    /**
+     * Геттер для поля field2.
+     *
+     * @return значение поля field2.
+     */
+    public SomeOtherInterface getField2() {
+        return field2;
     }
 }
