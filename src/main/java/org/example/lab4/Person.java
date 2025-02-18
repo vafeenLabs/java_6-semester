ce613a44c54665ecd0537921b9094516483e832b
Vafeen <666av6@gmail.com>
2025-02-18 17:37:50 +0300

    Add Person

---
+++ src/main/java/org/example/lab4/Person.java
@@ -0,0 +1,17 @@
+package org.example.lab4;
+
+/**
+ * Класс для представления человека.
+ */
+class Person {
+    /**
+     * @param id         уникальный идентификатор человека.
+     * @param name       имя человека.
+     * @param gender     пол человека ("Male" или "Female").
+     * @param department подразделение, в котором работает человек.
+     * @param salary     зарплата человека.
+     * @param birthDate  дата рождения человека в формате "dd.MM.yyyy".
+     */
+    public Person(int id, String name, String gender, String department, double salary, String birthDate) {
+    }
+}
